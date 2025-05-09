720eaa5c4629eba9de5238542fc84d7faec2d0ea
shark <sharksharkgo@outlook.com>
2025-05-09 23:15:29 +0800

    [Update] (astrolsp): 更新格式化选项和服务器配置
    重构配置以禁用全局保存自动格式化
    将服务器列表简化为包含 prolog_lsp 和 verible
    优化了 prolog_lsp 的启动命令和文件类型设置

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,18 +1,16 @@
 return {
   "AstroNvim/astrolsp",
-  -- we need to use the function notation to get access to the `lspconfig` module
-  ---@param opts AstroLSPOpts
-  opts = function(plugin, opts)
-    -- insert "prolog_lsp" into our list of servers
-    opts.servers = opts.servers or {}
-    table.insert(opts.servers, "prolog_lsp")
-    table.insert(opts.servers, "verible")
-
-    -- extend our configuration table to have our new prolog server
-    opts.config = require("astrocore").extend_tbl(opts.config or {}, {
-      -- this must be a function to get access to the `lspconfig` module
+  ---@type AstroLSPOpts
+  opts = {
+    formatting = {
+      format_on_save = {
+        enabled = false, -- 全局关闭保存自动格式化
+        ignore_filetypes = { "markdown", "verilog", "systemverilog" },
+      },
+    },
+    servers = { "prolog_lsp", "verible" },
+    config = {
       prolog_lsp = {
-        -- the command for starting the server
         cmd = {
           "swipl",
           "-g",
@@ -24,11 +22,9 @@ return {
           "--",
           "stdio",
         },
-        -- the filetypes to attach the server to
         filetypes = { "prolog" },
-        -- root directory detection for detecting the project root
         root_dir = require("lspconfig.util").root_pattern("pack.pl"),
       },
-    })
-  end,
+    },
+  },
 }
