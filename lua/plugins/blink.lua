6081c2c47faddbf326a1253b21f7c3ea0af6cdc3
shark <sharksharkgo@outlook.com>
2025-05-13 23:05:58 +0800

    [Enhancement] (blink): 更新补全键盘映射和源优先级
    添加了自定义键盘映射以改进文档显示与选择功能
    优化了补全源的配置和优先级设置
    确保预构建二进制文件的版本不匹配被忽略

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,41 +1,52 @@
 return {
   "Saghen/blink.cmp",
   opts = function(_, opts)
-    opts.keymap = { preset = 'default' }
+    opts.keymap = {
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-N>"] = { "select_next", "show" },
+      ["<C-P>"] = { "select_prev", "show" },
+      ["<C-J>"] = { "select_next", "fallback" },
+      ["<C-K>"] = { "select_prev", "fallback" },
+      ["<C-U>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-D>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = {
+        "select_next",
+        "snippet_forward",
+        function(cmp)
+          if vim.fn.col('.') > 1 or vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+        end,
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        "select_prev",
+        "snippet_backward",
+        function(cmp)
+          if vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+        end,
+        "fallback",
+      },
+    }
+
     opts.appearance = { nerd_font_variant = 'mono' }
     opts.completion = { documentation = { auto_show = false } }
 
-    -- 新增 fuzzy.prebuilt_binaries 配置
     opts.fuzzy = opts.fuzzy or {}
     opts.fuzzy.prebuilt_binaries = opts.fuzzy.prebuilt_binaries or {}
     opts.fuzzy.prebuilt_binaries.ignore_version_mismatch = true
 
-    -- 设置默认启用的补全源
     opts.sources = opts.sources or {}
     opts.sources.default = { "lsp", "path", "snippets", "buffer", "cmdline" }
 
-    -- 设置各补全源的优先级和显示名称
     opts.sources.providers = vim.tbl_deep_extend("force", opts.sources.providers or {}, {
-      lsp = {
-        name = "LSP",
-        score_offset = 0,
-      },
-      path = {
-        name = "Path",
-        score_offset = 1,
-      },
-      snippets = {
-        name = "Snippets",
-        score_offset = -1,
-      },
-      buffer = {
-        name = "Buffer",
-        score_offset = -2,
-      },
-      cmdline = {
-        name = "Cmdline",
-        score_offset = -1,
-      },
+      lsp = { name = "LSP", score_offset = 0 },
+      path = { name = "Path", score_offset = 1 },
+      snippets = { name = "Snippets", score_offset = -1 },
+      buffer = { name = "Buffer", score_offset = -2 },
+      cmdline = { name = "Cmdline", score_offset = -1 },
     })
 
     return opts
